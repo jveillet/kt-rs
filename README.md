446ab06b05556f1cf72aca1e3b66fa36fc001beb
Jérémie Veillet <jveillet@users.noreply.github.com>
2019-02-25 16:20:56 +0100

    Initial Release (#1)
    * Initial Release
    * Fix documentation headers

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # kt-rs
+
+A drop-in cat replacement written in Rust.
+
+## Disclaimer
+
+This project is purely educationnal and serve as an example for a blog post on [demainilpleut](https://www.demainilpleut.fr/).
+
+It is not mean to be a fully functionnal software and does not come with any guarantees of some sort.
+
+## Prerequisites
+
+This project uses `Rust` and `Cargo`, so make sure that you have the [Rust toolchain](https://rustup.rs/) installed on your environment.
+
+## Installation
+
+```
+$ cd Development/
+$ git clone git@github.com:jveillet/kt-rs.git
+$ cd kt-rs/
+$ cargo build --release
+$ cargo install --path .
+```
+
+## Usage
+
+```
+$ kt --help
+kt 0.1.0
+Jérémie Veillet. @jveillet
 A drop-in cat replacement written in Rust
+
+USAGE:
+    kt [FILE]
+
+FLAGS:
+    -h, --help       Prints help information
+    -V, --version    Prints version information
+
+ARGS:
+    <FILE>    File to print.
+```
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+extern crate clap;
+
+use clap::{Arg, App};
+use std::path::Path;
+use std::process;
+use std::fs::File;
+use std::io::{Read, Write};
+
+fn main() {
+    let matches = App::new("kt")
+      .version("0.1.0")
+      .author("Jérémie Veillet. @jveillet")
+      .about("A drop-in cat replacement written in Rust")
+      .arg(Arg::with_name("FILE")
+            .help("File to print.")
+            .empty_values(false)
+        )
+      .get_matches();
+
+    if let Some(file) = matches.value_of("FILE") {
+        if Path::new(&file).exists() {
+            match File::open(file) {
+                Ok(mut f) => {
+                    let mut data = String::new();
+                    f.read_to_string(&mut data).expect("[kt Error] Unable to read the  file.");
+                    let stdout = std::io::stdout(); // get the global stdout entity
+                    let mut handle = std::io::BufWriter::new(stdout); // optional: wrap that handle in a buffer
+                    match writeln!(handle, "{}", data) {
+                        Ok(_res) => {},
+                        Err(err) => {
+                            eprintln!("[kt Error] Unable to display the file contents. {:?}", err);
+                            process::exit(1);
+                        },
+                    }
+                }
+                Err(err) => {
+                    eprintln!("[kt Error] Unable to read the file. {:?}", err);
+                    process::exit(1);
+                },
+            }
+        }
+        else {
+            eprintln!("[kt Error] No such file or directory.");
+            process::exit(1);
+        }
+    }
+}
