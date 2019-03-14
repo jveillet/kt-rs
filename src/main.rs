fe76e725ed8c0ad9795405c8834ba042d5ddcad8
Jeremie Veillet <jeremie.veillet@gmail.com>
2019-03-14 13:56:45 +0100

    Formating wit cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,32 @@
 extern crate clap;
 
-use clap::{Arg, App};
+use clap::{App, Arg};
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::io::Write;
 use std::path::Path;
 use std::process;
-use std::io::prelude::*;
-use std::fs::File;
-use std::io::{Write};
-use std::io::BufReader;
 
 fn main() {
     let matches = App::new("kt")
-      .version("0.1.0")
-      .author("Jérémie Veillet. @jveillet")
-      .about("A drop-in cat replacement written in Rust.")
-      .arg(Arg::with_name("FILE")
-            .help("Concatenate FILE to standard output")
-            .empty_values(false)
+        .version("0.1.0")
+        .author("Jérémie Veillet. @jveillet")
+        .about("A drop-in cat replacement written in Rust.")
+        .arg(
+            Arg::with_name("FILE")
+                .help("Concatenate FILE to standard output")
+                .empty_values(false),
         )
-      .arg(Arg::with_name("number")
-            .short("n")
-            .long("number")
-            .help("number all output lines")
-            .takes_value(false)
-            .requires("FILE")
+        .arg(
+            Arg::with_name("number")
+                .short("n")
+                .long("number")
+                .help("number all output lines")
+                .takes_value(false)
+                .requires("FILE"),
         )
-    .get_matches();
+        .get_matches();
 
     if let Some(file) = matches.value_of("FILE") {
         if Path::new(&file).exists() {
@@ -37,28 +39,32 @@ fn main() {
                     for line in reader.lines() {
                         line_counter += 1;
                         if matches.is_present("number") {
-                            writeln!(handle, "{} {}", line_counter, line.unwrap()).unwrap_or_else(|err| {
-                                    eprintln!("[kt Error] Unable to display the file contents. {:?}", err);
+                            writeln!(handle, "{} {}", line_counter, line.unwrap()).unwrap_or_else(
+                                |err| {
+                                    eprintln!(
+                                        "[kt Error] Unable to display the file contents. {:?}",
+                                        err
+                                    );
                                     process::exit(1);
-                                }
-                            )
-                        }
-                        else {
+                                },
+                            );
+                        } else {
                             writeln!(handle, "{}", line.unwrap()).unwrap_or_else(|err| {
-                                    eprintln!("[kt Error] Unable to display the file contents. {:?}", err);
-                                    process::exit(1);
-                                }
-                            )
+                                eprintln!(
+                                    "[kt Error] Unable to display the file contents. {:?}",
+                                    err
+                                );
+                                process::exit(1);
+                            });
                         }
-                    } 
+                    }
                 }
                 Err(err) => {
                     eprintln!("[kt Error] Unable to read the file. {:?}", err);
                     process::exit(1);
-                },
+                }
             }
-        }
-        else {
+        } else {
             eprintln!("[kt Error] No such file or directory.");
             process::exit(1);
         }
